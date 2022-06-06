2e8e1a51d0bce2d3ec2220d833dd5e4e1a3fd9c7
Neo Yeung <dsmkai@gmail.com>
2022-06-06 16:45:22 +0800

    add prettier

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-import Knex from "knex";
+import Knex from 'knex';
 
-const knexConfigs = require("./knexfile");
-const configMode = process.env.NODE_ENV || "development";
+const knexConfigs = require('./knexfile');
+const configMode = process.env.NODE_ENV || 'development';
 const knex = Knex(knexConfigs[configMode]);
 
 async function main() {
-    console.log('Hello world!');
-    knex.destroy();
+  console.log('Hello world!');
+  knex.destroy();
 }
 
 main();
