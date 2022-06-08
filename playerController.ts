78cee485bf12ac1398c141dc2f8ab8c48ea8390d
Zero Lam <zeroolam@gmail.com>
2022-06-08 18:09:28 +0800

    finish server mvc

--- playerController.ts
+++ playerController.ts
@@ -2,20 +2,19 @@ import { Request, Response } from 'express';
 import { PlayerService } from './playerService';
 
 export class PlayerController {
-    // constructor(private playerService: PlayerService) { }
     private playerService: PlayerService;
     constructor(playerService: PlayerService) {
         this.playerService = playerService;
     }
 
     record = async (req: Request, res: Response) => {
-        let playerName = req.body;
+        let { playerName, score } = req.body;
         if (!playerName) {
             res.status(400).json({ error: 'missing player name' });
             return;
         }
-        // any limitation imposed on the player name???
+        // any limitation imposed on the player name??????????
 
-        await this.playerService.record(playerName);
+        this.playerService.record(playerName, score);
     }
 }
