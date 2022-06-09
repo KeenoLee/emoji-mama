20d7c6d94c76cb80ef73b9cb7be48f462756e676
Tsz Fung Ko <john.doe@gmail.com>
2022-06-09 18:45:36 +0800

    doing server of game page

--- routes.ts
+++ routes.ts
@@ -1,21 +1,27 @@
 import express from 'express';
 import { PlayerService } from './playerService';
 import { PlayerController } from './playerController';
+import { SinglePlayController } from './singlePlayController';
+
 
 let app = express();
 
 let routes = express.Router();
 let playerService = new PlayerService();
 let playerController = new PlayerController(playerService);
+let singlePlayController = new SinglePlayController();
 
 routes.post('/record', playerController.record)
 
+routes.post('/sendImage', singlePlayController.sendImage)
 // app.get('/', (req, res) => {
 //     console.log('Server is connected');
 //     res.end('Hello from express');
 // });
 
 const port = 8100;
+
+express.static('public')
 
 app.listen(port, () => {
     console.log('listening at http://localhost:' + port)
