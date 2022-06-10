d4fbeb7422035b3bca00892ab6e0ea86f8b33ffe
Tsz Fung Ko <john.doe@gmail.com>
2022-06-10 11:29:05 +0800

    finished get cookie

--- routes.ts
+++ routes.ts
@@ -1,18 +1,30 @@
 import express from 'express';
+import expressSession from 'express-session'
 import { PlayerService } from './playerService';
 import { PlayerController } from './playerController';
 import { SinglePlayController } from './singlePlayController';
 
 
 let app = express();
-
+declare module 'express-session' {
+    interface SessionData {
+        cookie: Cookie
+        grant?: any
+    }
+}
+export let sessionMiddleware = expressSession({
+    secret: 'Project 2',
+    resave: true,
+    saveUninitialized: true,
+  })
+  
 // let routes = express.Router();
 let playerService = new PlayerService();
 let playerController = new PlayerController(playerService);
 let singlePlayController = new SinglePlayController();
 
+app.use(sessionMiddleware)
 app.post('/record', playerController.record)
-
 app.post('/sendImage', singlePlayController.sendImage)
 // app.get('/', (req, res) => {
 //     console.log('Server is connected');
