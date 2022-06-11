c76ffbf8454a328bb63154ad5fde5d6e9d38a632
Tsz Fung Ko <john.doe@gmail.com>
2022-06-11 21:58:23 +0800

    minor changes

--- multiPlayController.ts
+++ multiPlayController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
+
 
 export class MultiPlayController {
     constructor() {}
     sendRoom = (req: Request, res: Response) => {
-        res.render('room')
+        res.render('room',  { roomId: req.params.room })
         return
     }
+    enterRoom = (req: Request, res: Response) => {
+        res.redirect(`/${uuidv4()}`)
+    }
+
 }
