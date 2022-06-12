b8e1af4a9933cbb86d18f27ff8233f8c9d390675
Tsz Fung Ko <john.doe@gmail.com>
2022-06-12 21:50:51 +0800

    trying multiplay

--- routes.ts
+++ routes.ts
@@ -2,21 +2,26 @@ import express from 'express';
 import expressSession from 'express-session'
 import {Server as SocketIO} from 'socket.io'
 import http from 'http'
-import { ExpressPeerServer } from 'peer'
 import { knex } from './app'
 import { PlayerService } from './playerService';
 import { PlayerController } from './playerController';
 import { SinglePlayController } from './singlePlayController';
-// import { MultiPlayController } from './multiPlayController';
+// Try MultiPlay
 
 
 let app = express();
 const server = new http.Server(app)
-const io = new SocketIO(server)
+// const io = new SocketIO(server)
+const io = new SocketIO(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
 let playerService = new PlayerService(knex);
 let playerController = new PlayerController(playerService);
 let singlePlayController = new SinglePlayController();
-// let multiPlayController = new MultiPlayController();
 declare module 'express-session' {
     interface SessionData {
         cookie: Cookie
@@ -30,10 +35,20 @@ export let sessionMiddleware = expressSession({
 })
 
 // let routes = express.Router();
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id)
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded')
+    })
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit('callUser', {signal: data.signalData, from: data.from, name: data.name})
+    })
+    socket.on('answerCall', (data) => {
+        io.to(data.to).emit('callAccepted', data.signal)
+    })
 
-const peerServer = ExpressPeerServer(server, {
-    // debug: true,
-});
+})
+
 
 app.use(sessionMiddleware)
 app.post('/record', playerController.record)
