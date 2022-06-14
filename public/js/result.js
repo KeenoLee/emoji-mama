9dbf7f16ceae98b70b4a353fadff5627fe3ed4c9
Neo Yeung <dsmkai@gmail.com>
2022-06-14 12:34:50 +0800

    update erd, create sql-table-screenshots, doing result, singlePlay

--- public/js/result.js
+++ public/js/result.js
@@ -1,3 +1,30 @@
+let postTemplate = document.querySelector(".gallery-container");
+postTemplate.remove();
+
+
 document.querySelector('.play-again').addEventListener("click", () => {
+    //TODO: clear indexedDB
     location.href = './index.html'
 })
+
+function showPost(post) {
+    const postContainer = createPost(post)
+    postList.appendChild(postContainer);
+
+}
+
+
+    // store.put({ id: 1, screenshots: '123' })
+
+    // const idQuery = store.get(1)
+
+    // idQuery.onsuccess = function () {
+    //     console.log('idQuery', store.get(1).result)
+    // }
+
+function createPost(post) {
+    let postContainer = postTemplate.cloneNode(true);
+    postContainer.querySelector(".gallery").textContent = 'FIXME:'
+    return postContainer;
+}
+getImageFromIndexedDB()
