69d586443b410262c87a865f7a8184d11a07146a
Zero Lam <zeroolam@gmail.com>
2022-06-15 15:15:34 +0800

    add ranking page

---
+++ public/js/ranking.js
@@ -0,0 +1,13 @@
+const names = document.querySelectorAll('.names');
+const scores = document.querySelectorAll('.scores');
+
+fetch('/rank')
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.topTen)
+        for (let i = 0; i < json.topTen.length; i++) {
+            names[i].textContent = json.topTen[i].name;
+            scores[i].textContent = json.topTen[i].score;
+        }
+    })
+    .catch(error => console.error('error: ', error));
